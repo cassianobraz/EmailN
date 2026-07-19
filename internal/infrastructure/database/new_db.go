7d517d087cf5c0a50b884ea3bbed929386c4c717
cassianobraz <cassanopb@gmail.com>
2026-07-19 01:47:35 -0400

    feat: variable database

--- internal/infrastructure/database/new_db.go
+++ internal/infrastructure/database/new_db.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"os"
+
 	"emailn/internal/domain/campaign"
 
 	"gorm.io/driver/postgres"
@@ -8,7 +10,7 @@ import (
 )
 
 func NewDB() *gorm.DB {
-	dsn := "host=localhost user=emailn_dev password=d4#rt6 dbname=emailn_dev port=5432 sslmode=disable"
+	dsn := os.Getenv("DATABASE")
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("fail to connect to database")
